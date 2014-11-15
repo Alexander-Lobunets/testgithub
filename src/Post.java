393bc2ae54cfe96c6d2e775821cc15ab1563d82e
Shur4eG <lobunets@gmail.com>
2014-11-15 15:43:23 +0200

    yttjg

---
+++ src/Post.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by lobunets on 15.11.2014.
+ */
+public abstract class Post {
+    String caption;
+    String body;
+    Date postdate;
+    List<String> comments;
+
+    Post(){};
+
+
+}
