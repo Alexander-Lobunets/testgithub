11a72187276d866c2df4ba1a3ddf5f7c9a24f6f2
Alexander Lobunets <alexander.lobunets@hp.com>
2014-11-20 11:28:23 +0200

    added delete post

--- src/ua/com/lohika/bsm/qa/blog/Blog.java
+++ src/ua/com/lohika/bsm/qa/blog/Blog.java
@@ -11,12 +11,17 @@ public class Blog {
 
 
     private List<Record> records;
+    private Scanner sc;
+
     public List<Record> getRecords() {
         return records;
     }
 
     public void setRecords(List<Record> records) {
         this.records = records;
+    }
+    private Record getRecord(int id){
+        return records.get(id);
     }
 
     public static void main(String[] args){
@@ -25,9 +30,7 @@ public class Blog {
     }
 
     // return record
-    Record getRecord(int id){
-        return records.get(id);
-    }
+
 
     public void mainMenu(){
 
@@ -42,75 +45,32 @@ public class Blog {
     }
 
     public void mainMenuSwitch(){
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
         String postAction = null;
         postAction = sc.next();
         switch (postAction){
             case "CREATE":
-                System.out.println("Type post title");
-                String title = sc.nextLine();
-                sc.nextLine();
-                System.out.println("Type post body text");
-                String body = sc.nextLine();
-                System.out.println("Type your username");
-                String username = sc.nextLine();
-                List<Attachment> attachments = new ArrayList<>();
-                Record post = new Post(title, body, username, attachments);
-
-                System.out.println(records);
-                records.add(post);
-                System.out.println(records);
-                System.out.println("The post created successfully");
-
+                createPost();
                 break;
 
             case "GET":
-                System.out.println("Please enter the specific Post ID ");
-                int id = 0;
-//                while(!sc.hasNextInt()){
-//                    System.out.println("Please enter the Post ID. Ex.: 1");
-////                    id = sc.nextInt();
-//                }
-
-//                System.out.println("ID = " + id);
-
-                boolean incorrectID = true;
-                while(incorrectID) {
-                    id = sc.nextInt();
-                    if ((records.size() - 1) < id) {
-                        System.out.printf("The post with ID %d doesn't exist.\n", id);
-                    } else {
-                        incorrectID = false;
-                    }
-                }
-                Post postget = (Post) records.get(id);
-                System.out.println("Title: " + postget.getTitle());
-                System.out.println("Body: " + postget.getBody());
-                System.out.println("Date: + " + postget.getRecordDate());
-                System.out.println("Created by " + postget.getUsername());
-                /** must be implemented later
-                 * System.out.println("Comments:"  );
-                System.out.println("Attachments: "); */
-
+                getPostByID();
                 break;
 
             case "EDIT":
+                //not implemented yet
                 System.out.println("edit");
 
             case "DELETE":
-                System.out.println("delete");
+                deletePostByID();
                 break;
 
             case "ALL":
-//                System.out.println("all");
-
-
+                //not implemented yet
                 Iterator<Record> iterRecords = records.iterator();
                 while(iterRecords.hasNext()){
                     System.out.println(iterRecords.next());
                 }
-
-
                 break;
 
             case "EXIT":
@@ -125,7 +85,78 @@ public class Blog {
 
     }
 
-    void go(){
+
+
+    private void createPost(){
+        System.out.println("Type post title");
+        String title = sc.nextLine();
+        sc.nextLine();
+        System.out.println("Type post body text");
+        String body = sc.nextLine();
+        System.out.println("Type your username");
+        String username = sc.nextLine();
+        List<Attachment> attachments = new ArrayList<>();
+        Record post = new Post(title, body, username, attachments);
+
+        System.out.println(records);
+        records.add(post);
+        System.out.println(records);
+        System.out.println("The post created successfully");
+    }
+
+    private void getPostByID(){
+        System.out.println("Please enter the specific Post ID ");
+        int id = 0;
+//                while(!sc.hasNextInt()){
+//                    System.out.println("Please enter the Post ID. Ex.: 1");
+////                    id = sc.nextInt();
+//                }
+
+//                System.out.println("ID = " + id);
+
+        boolean incorrectID = true;
+        while(incorrectID) {
+            id = sc.nextInt();
+            if ((records.size() - 1) < id) {
+                System.out.printf("The post with ID %d doesn't exist.\n", id);
+            } else {
+                incorrectID = false;
+            }
+        }
+        Post postget = (Post) records.get(id);
+        System.out.println("Title: " + postget.getTitle());
+        System.out.println("Body: " + postget.getBody());
+        System.out.println("Date: + " + postget.getRecordDate());
+        System.out.println("Created by " + postget.getUsername());
+        /** must be implemented later
+         * System.out.println("Comments:"  );
+         System.out.println("Attachments: "); */
+
+    }
+
+
+    private void deletePostByID(){
+        System.out.println("Please enter the specific Post ID ");
+        int id = 0;
+        boolean incorrectID = true;
+        while(incorrectID) {
+            id = sc.nextInt();
+            if ((records.size() - 1) < id) {
+                System.out.printf("The post with ID %d doesn't exist.\n", id);
+            } else {
+                incorrectID = false;
+            }
+        }
+        records.remove(id);
+        System.out.printf("The Post with ID %d deleted", id );
+
+    }
+
+
+
+
+
+    private void go(){
         records = new ArrayList<>();
         while (true){
 
